b5b1a3a8a23e0b4289948848f2a8ec01c04d3a1f
Alexandre FLAMENT <alexflam2003@gmail.com>
2023-04-11 16:20:53 +0200

    clear_buffer

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 int running = 1;
 int c;
 
+void clear_buffer() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 void optionA(ShapeNode *shape_list) {
     int choice = 0;
     while (choice < 1 || choice > 7 ) {
@@ -19,6 +24,7 @@ void optionA(ShapeNode *shape_list) {
         printf("        7. Return to previous menu\n");
         printf("     >> Your choice: ");
         scanf("%i", &choice);
+        clear_buffer();
         printf("\n");
     }
     int x, y, x1, y1, r, w, h, l;
@@ -26,38 +32,48 @@ void optionA(ShapeNode *shape_list) {
         case 1:
             printf("         >> Enter the x and y coordinates: ");
             scanf("%i %i", &x, &y);
+            clear_buffer();
             add_shape_to_node(shape_list, create_point_shape(x,y));
             break;
         
         case 2:
             printf("         >> Enter the x and y coordinates of the first point: ");
             scanf("%i %i", &x, &y);
+            clear_buffer();
             printf("         >> Enter the x and y coordinates of the second point: ");
             scanf("%i %i", &x1, &y1);
+            clear_buffer();
             add_shape_to_node(shape_list, create_line_shape(x,y,x1,y1));
             break;
         
         case 3:
             printf("         >> Enter the x and y coordinates of the origin: ");
             scanf("%i %i", &x, &y);
+            clear_buffer();
             printf("         >> Enter the radius of the circle: ");
             scanf("%i", &r);
+            clear_buffer();
             add_shape_to_node(shape_list, create_circle_shape(x,y,r));
             break;
         case 4:
             printf("         >> Enter the x and y coordinates of the top left point: ");
             scanf("%i %i", &x, &y);
+            clear_buffer();
             printf("         >> Enter the length: ");
             scanf("%i", &l);
+            clear_buffer();
             add_shape_to_node(shape_list, create_square_shape(x,y,l));
             break;
         case 5:
             printf("         >> Enter the x and y coordinates of the top left point: ");
             scanf("%i %i", &x, &y);
+            clear_buffer();
             printf("         >> Enter the width: ");
             scanf("%i", &w);
+            clear_buffer();
             printf("         >> Enter the height: ");
             scanf("%i", &h);
+            clear_buffer();
             add_shape_to_node(shape_list, create_rect_shape(x, y, w, h));
             break;
         case 6:
@@ -67,6 +83,7 @@ void optionA(ShapeNode *shape_list) {
             while (running) {
                 printf("         >> Enter the x and y coordinates the point %i: ", counter);
                 scanf("%i %i", &x, &y);
+                clear_buffer();
                 points[counter] = create_point(x,y);
                 counter++;
                 if (points[0]->x == points[counter-1]->x && points[0]->y == points[counter-1]->y && counter>1) {
@@ -94,9 +111,10 @@ int main() {
         printf("    E. Help (WIP)\n");
         printf("    F. Exit\n");
         printf(" >> Your choice: ");
-        scanf("%c[¨n]", &choice);
+        scanf("%c", &choice);
+        clear_buffer();
         printf("\n");
-        
+
         switch (choice) {
             case 'F':
             case 'f':
@@ -127,6 +145,7 @@ int main() {
         if (choice == 'B' || choice == 'b' || choice == 'D' || choice == 'd') {
             printf("Press enter to continue...");
             scanf("%c", &c);
+            clear_buffer();
         }
     }
 
