28d42eca524abe6664bda05f7684c7ae37d6fe2b
Alexandre Flament <alexflam2003@gmail.com>
2023-04-20 18:49:41 +0200

    pixel_line function done. Unknown if it works, will have to check when display done.

--- area.c
+++ area.c
@@ -85,7 +85,8 @@ void delete_pixel(Pixel* pixel) {
 // PIXEL DRAWING
 
 Pixel **pixel_point(Shape *shp, int *nb_pixels) {
-    Pixel *p = create_pixel(shp->ptrShape->x, shp->ptrShape->y);
+    Point *pt = (Point*)shp->ptrShape;
+    Pixel *p = create_pixel(pt->x, pt->y);
     Pixel **pixel_tab = malloc(sizeof(Pixel*));
     pixel_tab[0] = p;
     *nb_pixels = 1;
@@ -93,6 +94,55 @@ Pixel **pixel_point(Shape *shp, int *nb_pixels) {
 }
 
 Pixel **pixel_line(Shape *shp, int *nb_pixels) {
-    Point pbegin = shp->ptrShape->p1;
-    Point pend = shp->ptrShape->p2;
+    Line *ln = (Line*)shp->ptrShape;
+
+    int dx = ln->p2->x - ln->p1->x;
+    int dy = ln->p2->y - ln->p1->y;
+
+    int x,y;
+    if (dx > 0) {
+        int x = ln->p1->x;
+        int y = ln->p1->y;
+    } else {
+        int x = ln->p2->x;
+        int y = ln->p2->y;
+    }
+
+    int dmin, dmax;
+    if (dx < abs(dy)) {
+        dmin = dx;
+        dmax = abs(dy);
+    } else {
+        dmin = abs(dy);
+        dmax = dx;
+    }
+
+    int nb_seg = dmin + 1;
+    int nb_pts = (dmax + 1) / nb_seg;
+    int remaining = (dmax + 1) % nb_seg;
+    *nb_pixels = nb_seg * nb_pts + remaining;
+
+    Pixel **pixel_tab = malloc(*nb_pixels * sizeof(Pixel*));
+
+    int dx_step = dx / dmax;
+    int dy_step = dy / dmax;
+    int i = 0;
+    for (i = 0; i < nb_pts; i++) {
+        pixel_tab[i] = create_pixel(x, y);
+        x += dx_step * nb_seg;
+        y += dy_step * nb_seg;
+        for (int j = 1; j < nb_seg; j++) {
+            pixel_tab[i * nb_seg + j] = create_pixel(x, y);
+            x += dx_step;
+            y += dy_step;
+        }
+    }
+
+    for (int j = 0; j < remaining; j++) {
+        pixel_tab[i * nb_seg + j] = create_pixel(x, y);
+        x += dx_step;
+        y += dy_step;
+    }
+
+    return pixel_tab;
 }
